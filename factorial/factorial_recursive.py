ea152928dfa9c8f5523dbd0ce3c4be6448523527
imjonathanjackson <imjonathanjackson@gmail.com>
2025-01-11 22:38:44 -0800

    Update factorial_recursive.py

--- factorial/factorial_recursive.py
+++ factorial/factorial_recursive.py
@@ -1,6 +1,11 @@
 def factorial_recursive(n):
+  #Convert string input to integer
+  n = int (n)
   #Base Case
   if n == 0:
-    return = 1
-  else
-    return n * factorial_recursive(n-1)
+    return 1
+  else:
+    return n * factorial_recursive(n - 1)
+  
+fact = input("Enter number to calculate factorial of: ")
+print(factorial_recursive(fact))
